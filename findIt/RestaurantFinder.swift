7bf172945f142e3bab51f842a019b4c67e0adf17
Raul Martinez-Luna <raul.martinez.luna.2005@gmail.com>
2021-07-20 10:49:24 -0700

    Created a new file, which implements the Yelp API in order to find a restaurant. So far I have only added a Singleton and the App details, it is currently missing the search functionality. I will add those once I understand how to use them.

---
+++ findIt/RestaurantFinder.swift
@@ -0,0 +1,19 @@
+//
+//  RestaurantFinder.swift
+//  findIt
+//
+//  Created by Raul Martinez-Luna on 7/20/21.
+//
+
+import Foundation
+
+class RestaurantFinder {
+    //Singleton
+    static let rFinder = RestaurantFinder()
+    
+    //App Details
+    let appID: String = "C0xxAUOOVtQtFGLzE_1xkQ"
+    let appKey: String = "H4c-V9MkkqMjxAHQWHHNljdKC8O6v3eUNfkeyXj7c3i7_TLHDJY3gbC9nVuokcXcjnFS9nL8ClSV6eZa885ckdCwEuFWX0uTEkRm3JboUDo_lpceLUYo7F472MH1YHYx"
+    
+    //Search - Not Completed -
+}
