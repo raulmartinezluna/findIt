fbca315f04c590308ce006a1be36c6e3aa802c7c
Raul Martinez Luna <raul.martinez.luna.2005@gmail.com>
2021-07-29 17:54:42 -0700

    Added Button Capabilities for the Restaurant Details Page

--- findIt/RestaurantDetailViewController.swift
+++ findIt/RestaurantDetailViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import CDYelpFusionKit
 import MapKit
 import CoreLocation
+import Kingfisher
 
 
 class RestaurantDetailViewController: UIViewController {
@@ -19,8 +20,34 @@ class RestaurantDetailViewController: UIViewController {
         super.viewDidLoad()
         yelpButton.imageView?.image = UIImage.yelpLogo()
         nameLabel.text = business?.name ?? ""
-        restaurantImageView.image = UIImage.yelpStars(numberOfStars: .twoHalf, forSize: .large)
-        restaurantImageView.contentMode = .scaleAspectFit
+        restaurantImageView.kf.setImage(with: URL(string: business?.imageUrl ?? ""))
+        restaurantImageView.contentMode = .scaleAspectFill
+        var numberOfStars = CDYelpStars.zero
+        switch business?.rating ?? 0 {
+        case 0...0.5:
+            numberOfStars = CDYelpStars.zero
+        case 0.5...1:
+            numberOfStars = CDYelpStars.oneHalf
+        case 1...1.5:
+            numberOfStars = CDYelpStars.one
+        case 1.5...2:
+            numberOfStars = CDYelpStars.two
+        case 2...2.5:
+            numberOfStars = CDYelpStars.twoHalf
+        case 2.5...3:
+            numberOfStars = CDYelpStars.three
+        case 3...3.5:
+            numberOfStars = CDYelpStars.threeHalf
+        case 3.5...4:
+            numberOfStars = CDYelpStars.four
+        case 4...4.5:
+            numberOfStars = CDYelpStars.fourHalf
+        case 4.5...5:
+            numberOfStars = CDYelpStars.five
+        default:
+            numberOfStars = CDYelpStars.zero
+        }
+        reviewButton.setImage(UIImage.yelpStars(numberOfStars: numberOfStars, forSize: .large), for: .normal)
         // Do any additional setup after loading the view.
         print(business?.name, "detail")
         print(business?.rating)
@@ -40,6 +67,20 @@ class RestaurantDetailViewController: UIViewController {
     @IBOutlet weak var restaurantImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var starImage: UIImageView!
+    @IBOutlet weak var reviewButton: UIButton!
+    
+    
+    @IBAction func callPressed(_ sender: Any) {
+        let phone = business?.phone ?? ""
+        guard let number = URL(string: "tel://" + phone) else { return }
+        UIApplication.shared.open(number)
+    }
+    
+    @IBAction func webPressed(_ sender: Any) {
+        let website = business?.url ?? ""
+        guard let site = URL(string: website) else { return }
+        UIApplication.shared.open(site)
+    }
     /*
     // MARK: - Navigation
 
